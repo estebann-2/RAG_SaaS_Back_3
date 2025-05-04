9f816227822edbb2c808f352d3743d2e3412d7be
Esteban Caicedo <esteban.caicedo@vml.com>
2025-05-03 19:55:30 -0500

    Update settings.py

--- RAG_SaaS/settings.py
+++ RAG_SaaS/settings.py
@@ -20,6 +20,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# OpenAI API Key
+OPENAI_API_KEY = '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'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
