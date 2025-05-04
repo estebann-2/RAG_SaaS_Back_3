d6705b8a9b7074acc303aae09374ac896601f4d8
Esteban Caicedo <esteban.caicedo@vml.com>
2025-05-03 21:09:10 -0500

    Update settings.py

--- RAG_SaaS/settings.py
+++ RAG_SaaS/settings.py
@@ -92,10 +92,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rag_saas_back',
-        'USER': 'uniandes',
-        'PASSWORD': 'IlzjpcP]y5qC.0va',
-        'HOST': '34.70.33.208',
-        'PORT': '3306',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
